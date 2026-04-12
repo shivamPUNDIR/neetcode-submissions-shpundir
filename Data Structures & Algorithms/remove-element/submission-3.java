262a684a7beb497e199bff3f90360d9171423c3c
shivamPUNDIR <47301925+shivamPUNDIR@users.noreply.github.com>
2026-04-12 19:21:17 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = 0;
+        
+        while(right > 0 && left < nums.length && nums[right] == val){
+            right-=1;
+        }
+        while(left <= right){
+            if(nums[left] == val){
+                // swap with right which could/could not be equal to val
+                nums[left] = nums[right];
+                nums[right] = val;
+                right--;
+            }else {
+                left++;
+            }
+        }
+        return right + 1;
+    }
+}
